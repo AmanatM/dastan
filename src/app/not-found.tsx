99c6439977af3ea1e6ad62b36fdd4b97180777a0
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-04 08:56:50 -0700

    fix: not fount client

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { unstable_setRequestLocale } from "next-intl/server";
 import Error from "next/error";
 
 // Render the default Next.js 404 page when a route
 // is requested that doesn't match the middleware and
 // therefore doesn't have a locale associated with it.
 
-export default function NotFound({ params: { locale } }: { params: { locale: string } }) {
-  unstable_setRequestLocale(locale);
+export default function NotFound() {
   return (
     <html lang="en">
       <body>
