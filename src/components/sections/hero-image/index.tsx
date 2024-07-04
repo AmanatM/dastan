8cf4895769e5460619d5b7094f2284623082c2bd
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-03 22:21:37 -0700

    hero text

--- src/components/sections/hero-image/index.tsx
+++ src/components/sections/hero-image/index.tsx
@@ -13,7 +13,7 @@ export function HeroImage() {
         <div className="h-3/6 lg:h-auto lg:w-4/6 relative">
           <Image quality={80} src="/images/hero.jpg" fill={true} objectFit="cover" alt="" />
         </div>
-        <div className="h-3/6 lg:h-auto lg:w-2/6 pt-[64px] flex items-center justify-start lg:p-20 p-4">
+        <div className="h-3/6 lg:h-auto lg:w-2/6 flex items-center justify-start lg:p-20 p-4">
           <div className="flex flex-col items-start justify-center gap-x-2 gap-y-4">
             <h1 className="flex lg:hidden font-bold text-3xl">DASTAN ABIKOV</h1>
             {["Entrepreneur", "Investor", "Speaker", "Philanthropist"].map((item, index) => (
@@ -25,7 +25,7 @@ export function HeroImage() {
           </div>
         </div>
       </div>
-      <h1 className="absolute bottom-0 left-0 right-0 font-bold text-[170px] text-center text-nowrap max-w-full hidden lg:flex">
+      <h1 className="absolute bottom-0 left-0 right-0 font-bold text-[170px] text-center text-nowrap max-w-full hidden lg:block">
         DASTAN ABIKOV
       </h1>
     </section>
