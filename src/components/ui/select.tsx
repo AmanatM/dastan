590bfabe35c6f47f599f13f00d81bea1fa5bdb55
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-04 12:02:34 -0700

    ui: remove ring on select component

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -20,7 +20,7 @@ const SelectTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
-      "ring-0 focus:ring-0", // Remove default focus styles
+      "!focus:ring-0 !ring-0", // Remove default focus styles
       className,
     )}
     {...props}
