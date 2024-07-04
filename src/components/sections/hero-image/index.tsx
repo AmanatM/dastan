067ac89e1d64383fb3400f2704411ddb4db155fc
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-03 22:17:27 -0700

    hero fix

--- src/components/sections/hero-image/index.tsx
+++ src/components/sections/hero-image/index.tsx
@@ -9,12 +9,13 @@ import Image from "next/image";
 export function HeroImage() {
   return (
     <section className="relative h-screen overflow-hidden">
-      <div className="flex size-full">
-        <div className="w-4/6 relative">
+      <div className="flex flex-col lg:flex-row size-full">
+        <div className="h-3/6 lg:h-auto lg:w-4/6 relative">
           <Image quality={80} src="/images/hero.jpg" fill={true} objectFit="cover" alt="" />
         </div>
-        <div className="w-2/6 pt-[64px] flex items-center justify-start p-3 pl-20">
+        <div className="h-3/6 lg:h-auto lg:w-2/6 pt-[64px] flex items-center justify-start lg:p-20 p-4">
           <div className="flex flex-col items-start justify-center gap-x-2 gap-y-4">
+            <h1 className="flex lg:hidden font-bold text-3xl">DASTAN ABIKOV</h1>
             {["Entrepreneur", "Investor", "Speaker", "Philanthropist"].map((item, index) => (
               <div key={index} className="flex items-center gap-x-2">
                 <div className="w-[8px] h-[8px] bg-accent"></div>
@@ -24,7 +25,7 @@ export function HeroImage() {
           </div>
         </div>
       </div>
-      <h1 className="absolute bottom-0 left-0 right-0 font-bold text-[170px] text-center text-nowrap max-w-full">
+      <h1 className="absolute bottom-0 left-0 right-0 font-bold text-[170px] text-center text-nowrap max-w-full hidden lg:flex">
         DASTAN ABIKOV
       </h1>
     </section>
