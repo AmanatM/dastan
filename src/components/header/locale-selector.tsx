16dc1487d333d369da792c287ce937a7f639a760
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-04 05:55:35 -0700

    locale flags

--- src/components/header/locale-selector.tsx
+++ src/components/header/locale-selector.tsx
@@ -1,15 +1,35 @@
 "use client";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 import { useTransition, useState, useEffect } from "react";
 import { useParams, usePathname, useRouter } from "next/navigation";
 import { locales } from "@/config";
 import { useLocale } from "next-intl";
+import { KGFlagIcon, RUFlagIcon, USFlagIcon } from "@/icons/flag-icons";
+
+type Language = {
+  name: string;
+  icon: JSX.Element;
+};
+
+const languages: Record<string, Language> = {
+  en: {
+    name: "English",
+    icon: <USFlagIcon />,
+  },
+  ru: {
+    name: "Русский",
+    icon: <RUFlagIcon />,
+  },
+  kg: {
+    name: "Кыргызча",
+    icon: <KGFlagIcon />,
+  },
+};
 
 function LocaleSelector() {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
-  const params = useParams();
   const currentLocale = useLocale();
 
   const [value, setValue] = useState(currentLocale);
@@ -27,15 +47,21 @@ function LocaleSelector() {
   }
 
   return (
-    <Select onValueChange={onSelectChange} value={value}>
+    <Select onValueChange={onSelectChange} value={value} disabled={isPending}>
       <SelectTrigger className="border-none outline-none bg-transparent uppercase">
-        <SelectValue />
+        <SelectValue>
+          <div className="flex items-center gap-x-2">
+            {languages[value].icon} {value}
+          </div>
+        </SelectValue>
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
           {locales.map((locale) => (
-            <SelectItem value={locale} key={locale} className="uppercase">
-              {locale}
+            <SelectItem value={locale} key={locale} className="uppercase cursor-pointer">
+              <div className="flex items-center gap-x-2">
+                {languages[locale].icon} {locale}
+              </div>
             </SelectItem>
           ))}
         </SelectGroup>
