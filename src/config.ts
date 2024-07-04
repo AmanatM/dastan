818deae2d9b3d3b529b0d1dc7c61e685a4686988
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-04 14:16:19 -0700

    local: removed /en on default language

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-import { Pathnames, LocalePrefix } from "next-intl/routing";
+import { Pathnames, LocalePrefix } from "next-intl/routing"
 
-export const defaultLocale = "en" as const;
-export const locales = ["en", "ru", "ky"] as const;
+export const defaultLocale = "en" as const
+export const locales = ["en", "ru", "ky"] as const
 
 export const pathnames: Pathnames<typeof locales> = {
   "/": "/",
@@ -10,6 +10,6 @@ export const pathnames: Pathnames<typeof locales> = {
     ru: "/ru",
     ky: "/ky",
   },
-};
+}
 
-export const localePrefix: LocalePrefix<typeof locales> = "always";
+export const localePrefix: LocalePrefix<typeof locales> = "as-needed"
