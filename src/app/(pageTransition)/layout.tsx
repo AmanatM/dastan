e38ae7000165b855a35822c7b4169aee5614c25c
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-16 07:56:52 -0700

    chore: added speedinsights

--- src/app/(pageTransition)/layout.tsx
+++ src/app/(pageTransition)/layout.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import PageTransitionEffect from "@/components/Transition"
 
@@ -10,6 +11,7 @@ export default function PageTransitionLayout({ children }: { children: React.Rea
       <body className={"min-h-svh max-w-[100vw] font-sans"}>
         <PageTransitionEffect>{children}</PageTransitionEffect>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
