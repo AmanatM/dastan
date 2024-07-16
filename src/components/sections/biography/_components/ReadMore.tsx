adb716db7849dd9badcdbfe061a83e5e720ca706
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-16 03:33:50 -0700

    fix: credenza overflow text scroll

---
+++ src/components/sections/biography/_components/ReadMore.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+  CredenzaContent,
+  CredenzaHeader,
+  CredenzaTitle,
+  CredenzaDescription,
+  CredenzaBody,
+  CredenzaFooter,
+  CredenzaClose,
+} from "@/components/ui/credenza"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { useTranslations } from "next-intl"
+
+function ReadMore() {
+  const t = useTranslations()
+  return (
+    <CredenzaContent className="max-w-2xl">
+      <CredenzaHeader>
+        <CredenzaTitle className="uppercase">{t("Biography.subtitle")}</CredenzaTitle>
+        <CredenzaDescription className="hidden">{t("Biography.subtitle")}</CredenzaDescription>
+      </CredenzaHeader>
+      <CredenzaBody>
+        <ScrollArea className="h-[400px] overflow-auto">
+          <div className="space-y-3 p-3">
+            <p>{t("Biography.readMore.paragraph1")}</p>
+            <p>{t("Biography.readMore.paragraph2")}</p>
+            <p>{t("Biography.readMore.paragraph3")}</p>
+            <p>{t("Biography.readMore.paragraph4")}</p>
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </CredenzaBody>
+      <CredenzaFooter>
+        <CredenzaClose asChild>
+          <Button>{t("Global.close")}</Button>
+        </CredenzaClose>
+      </CredenzaFooter>
+    </CredenzaContent>
+  )
+}
+
+export default ReadMore
--- src/components/sections/biography/index.tsx
+++ src/components/sections/biography/index.tsx
@@ -2,23 +2,12 @@ import { Heading } from "@/components/common/heading"
 import { Section } from "@/components/common/layout"
 import ImageCarousel from "@/components/ImageCarousel"
 import { Button } from "@/components/ui/button"
-import Image from "next/image"
 
 import slider1 from "./images/childhood.jpg"
 import slider2 from "./images/certificate.jpg"
 import { useTranslations } from "next-intl"
-import {
-  Credenza,
-  CredenzaTrigger,
-  CredenzaContent,
-  CredenzaHeader,
-  CredenzaTitle,
-  CredenzaDescription,
-  CredenzaBody,
-  CredenzaFooter,
-  CredenzaClose,
-} from "@/components/ui/credenza"
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { Credenza, CredenzaTrigger } from "@/components/ui/credenza"
+import ReadMore from "./_components/ReadMore"
 
 export default function Biography() {
   const t = useTranslations()
@@ -68,36 +57,9 @@ export default function Biography() {
               {t("Global.readMore")}
             </Button>
           </CredenzaTrigger>
-          <ReadMoreContent />
+          <ReadMore />
         </Credenza>
       </div>
     </Section>
-  )
-}
-
-function ReadMoreContent() {
-  const t = useTranslations()
-  return (
-    <CredenzaContent className="max-w-2xl">
-      <CredenzaHeader>
-        <CredenzaTitle className="uppercase">{t("Biography.subtitle")}</CredenzaTitle>
-        <CredenzaDescription className="hidden">{t("Biography.subtitle")}</CredenzaDescription>
-      </CredenzaHeader>
-      <CredenzaBody>
-        <ScrollArea className="flex h-[400px] max-h-[80vh] w-full">
-          <div className="space-y-3">
-            <p>{t("Biography.readMore.paragraph1")}</p>
-            <p>{t("Biography.readMore.paragraph2")}</p>
-            <p>{t("Biography.readMore.paragraph3")}</p>
-            <p>{t("Biography.readMore.paragraph4")}</p>
-          </div>
-        </ScrollArea>
-      </CredenzaBody>
-      <CredenzaFooter>
-        <CredenzaClose asChild>
-          <Button>{t("Global.close")}</Button>
-        </CredenzaClose>
-      </CredenzaFooter>
-    </CredenzaContent>
   )
 }
