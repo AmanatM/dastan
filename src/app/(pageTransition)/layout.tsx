3c1650e49a0bc4253914544311c23b73b39e99b0
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-16 00:04:14 -0700

    chore: added @vercel/analytics

--- src/app/(pageTransition)/layout.tsx
+++ src/app/(pageTransition)/layout.tsx
@@ -1,5 +1,6 @@
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/react"
 
 import PageTransitionEffect from "@/components/Transition"
 
@@ -8,6 +9,7 @@ export default function PageTransitionLayout({ children }: { children: React.Rea
     <html className={`${GeistSans.variable} ${GeistMono.variable} scroll-smooth`}>
       <body className={`min-h-svh max-w-[100vw] font-sans`}>
         <PageTransitionEffect>{children}</PageTransitionEffect>
+        <Analytics />
       </body>
     </html>
   )
