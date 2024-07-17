b119f589c8ba21fc3b909e6836829eaaea7d2c2e
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-16 20:02:32 -0700

    added inter font

--- src/app/(pageTransition)/layout.tsx
+++ src/app/(pageTransition)/layout.tsx
@@ -1,14 +1,23 @@
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import PageTransitionEffect from "@/components/Transition"
 
+import { Inter } from "next/font/google"
+import { cn } from "@/lib/utils"
+
+const inter = Inter({
+  subsets: ["latin", "cyrillic", "cyrillic-ext"],
+  display: "swap",
+  variable: "--font-inter",
+  fallback: ["system-ui", "arial", "sans-serif"],
+  preload: true,
+})
+
 export default function PageTransitionLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className={`${GeistSans.variable} ${GeistMono.variable} scroll-smooth`}>
-      <body className={"min-h-svh max-w-[100vw] font-sans"}>
+    <html className={`scroll-smooth ${inter.variable}`}>
+      <body className={cn("min-h-svh max-w-[100vw]")}>
         <PageTransitionEffect>{children}</PageTransitionEffect>
         <Analytics />
         <SpeedInsights />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,8 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -21,10 +20,8 @@ const config = {
     },
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
+        sans: ["var(--font-inter)"],
       },
-
       gridTemplateColumns: {
         header: "1fr max-content 1fr",
       },
@@ -85,6 +82,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+} satisfies Config
 
-export default config;
+export default config
