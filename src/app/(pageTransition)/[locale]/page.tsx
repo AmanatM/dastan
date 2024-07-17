c8756619a04f7fc1ad8de869e8c773bbee048517
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-17 08:48:10 -0700

    dynami import component

--- src/app/(pageTransition)/[locale]/page.tsx
+++ src/app/(pageTransition)/[locale]/page.tsx
@@ -4,12 +4,12 @@ import dynamic from "next/dynamic"
 import Hero from "@/components/sections/hero"
 import { Companies } from "@/components/sections/companies"
 
+const Biography = dynamic(() => import("@/components/sections/biography"))
+const FeaturesList = dynamic(() => import("@/components/sections/features"))
+const PublicationsSlider = dynamic(() => import("@/components/sections/publicationsSlider"))
+
 export default function Home({ params: { locale } }: { params: { locale: string } }) {
   unstable_setRequestLocale(locale)
-
-  const Biography = dynamic(() => import("@/components/sections/biography"))
-  const FeaturesList = dynamic(() => import("@/components/sections/features"))
-  const PublicationsSlider = dynamic(() => import("@/components/sections/publicationsSlider"))
 
   return (
     <div>
