1c7c54bbdd2af2d2d38e06e844d6ae2157846195
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-16 17:45:54 -0700

    removed unnecesary layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import { ReactNode } from "react"
 import "./globals.css"
 import { Metadata } from "next"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+import PageTransitionEffect from "@/components/Transition"
+import { cn } from "@/lib/utils"
+
+import { Inter as FontSans } from "next/font/google"
+
+const fontSans = FontSans({
+  subsets: ["latin", "cyrillic", "cyrillic-ext"],
+  variable: "--font-sans",
+})
 
 type Props = {
   children: ReactNode
@@ -18,5 +30,13 @@ export const metadata: Metadata = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children, params: { locale } }: Props) {
-  return children
+  return (
+    <html className={`scroll-smooth ${fontSans.variable} font-sans`}>
+      <body className={cn("min-h-svh max-w-[100vw]")}>
+        <PageTransitionEffect>{children}</PageTransitionEffect>
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  )
 }
