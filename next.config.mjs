6350f8eb4229cbd2a432359f0bd2d488f9521571
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-18 19:09:34 -0700

    added robots.txt

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin"
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["lucide-react", "@radix-ui/react-icons"],
+  },
+}
 
 export default withNextIntl(nextConfig)
