45901474e8890526b3e9e8d204de1f919c68c3c1
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-23 08:55:00 -0700

    added globe to economic fund

--- src/components/sections/economicProgram/index.tsx
+++ src/components/sections/economicProgram/index.tsx
@@ -1,18 +1,18 @@
 import { Section } from "@/components/common/layout"
-import Image from "next/image"
 
 import { useTranslations } from "next-intl"
 
-import un_img from "@public/images/hero/un.jpg"
 import { ButtonLink } from "@/components/ui/button-link"
 import { ReaderIcon } from "@radix-ui/react-icons"
+
+import { GlobeComponent } from "@/components/GlobeComponent"
 
 export default function EconomicPlanSection() {
   const t = useTranslations("EconomicProgram")
 
   return (
     <Section container="default" id={"economicProgram"} className="!py-0 !pb-14">
-      <div className="dark:border-dark-border dark:bg-dark-surface-secondary flex min-h-96 w-full max-w-full flex-col rounded-lg border border-border bg-card p-px sm:max-w-full md:w-full md:flex-row md:odd:flex-row-reverse xl:gap-16">
+      <div className="flex min-h-96 w-full max-w-full flex-col rounded-lg border-border p-px sm:max-w-full md:w-full md:flex-row md:odd:flex-row-reverse xl:gap-16">
         <div className="flex flex-col justify-between gap-8 p-5 pt-6 md:flex-1 md:p-10">
           <div className="flex flex-col items-start gap-y-2 md:gap-y-5">
             <h5 className="text-text-primary dark:text-dark-text-primary text-2xl font-medium md:text-3xl">
@@ -26,13 +26,7 @@ export default function EconomicPlanSection() {
           </ButtonLink>
         </div>
         <figure className="-order-1 p-2 md:order-1 md:h-auto md:w-[360px] lg:w-[480px] xl:w-[560px]">
-          <Image
-            src={un_img}
-            className="dark:border-dark-border block aspect-video h-[200px] w-full rounded-lg border border-border object-cover md:h-full"
-            height={374}
-            width={560}
-            alt={t("title")}
-          />
+          <GlobeComponent />
         </figure>
       </div>
     </Section>
