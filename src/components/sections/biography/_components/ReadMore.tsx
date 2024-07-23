45aadb7b4c8f1b63f78518c1969b22140c8e9bc6
Ama <24210834+AmanatM@users.noreply.github.com>
2024-07-23 05:13:57 -0700

    biography: changed list styles(read more)

--- src/components/sections/biography/_components/ReadMore.tsx
+++ src/components/sections/biography/_components/ReadMore.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/dialog"
 
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
 import { useMessages, useTranslations } from "next-intl"
 
 function ReadMore() {
@@ -22,7 +23,12 @@ function ReadMore() {
       key,
       time: t(`Biography.readMore.${key}.time`),
       description: t.rich(`Biography.readMore.${key}.description`, {
-        li: chunks => <li>{chunks}</li>,
+        li: chunks => (
+          <li key={key} className="flex items-start gap-x-2">
+            <div className="mt-2 !h-[7px] !w-[7px] shrink-0 bg-accent" />
+            {chunks}
+          </li>
+        ),
       }),
     }
   })
@@ -33,13 +39,15 @@ function ReadMore() {
         <DialogTitle className="uppercase">{t("Biography.subtitle")}</DialogTitle>
         <DialogDescription className="hidden">{t("Biography.subtitle")}</DialogDescription>
       </DialogHeader>
-      <ScrollArea className="h-[400px] overflow-auto">
-        {readMoreItems.map(item => (
-          <div key={item.key} className="my-4 flex items-start gap-x-2">
-            <div className="mt-2 !h-[8px] !w-[8px] shrink-0 bg-accent" />
-            <div className="w-[90px] whitespace-nowrap text-nowrap font-bold">{item.time}:</div>
-
-            <ul className="flex flex-1 list-disc flex-col gap-y-1">{item.description}</ul>
+      <ScrollArea className="h-[400px] overflow-auto px-1">
+        {readMoreItems.map((item, index) => (
+          <div key={item.key}>
+            <div className="my-4 flex items-start gap-x-2">
+              {/* <div className="mt-2 !h-[8px] !w-[8px] shrink-0 bg-accent" /> */}
+              <div className="w-[90px] whitespace-nowrap text-nowrap font-bold">{item.time}:</div>
+              <ul className="flex flex-1 flex-col gap-y-2">{item.description}</ul>
+            </div>
+            {index !== readMoreItems.length - 1 && <Separator />}
           </div>
         ))}
 
